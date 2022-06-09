68b70445c306d67ac404f097b0377388bcefba6c
Jamie Baxter <baxterjr@gmail.com>
2022-06-09 14:11:31 -0300

    set tina to temp branch

--- tina-admin/src/App.tsx
+++ tina-admin/src/App.tsx
@@ -4,7 +4,7 @@ import { TinaEditProvider, setEditing } from "tinacms/dist/edit-state";
 function App() {
   setEditing(true);
 
-  const branch = "tina";
+  const branch = "temp";
   const clientId = "11e2bb0d-e8ae-40e5-ab5a-005e63f52b30";
   const apiURL =
     process.env.NODE_ENV == "development"
