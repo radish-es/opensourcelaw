b53d835cd50e5fe3735687d40e5ab823c2528ddb
Jamie Baxter <baxterjr@gmail.com>
2022-06-09 19:56:38 -0300

    branch for tina prod

--- tina-admin/src/App.tsx
+++ tina-admin/src/App.tsx
@@ -4,7 +4,7 @@ import { TinaEditProvider, setEditing } from "tinacms/dist/edit-state";
 function App() {
   setEditing(true);
 
-  const branch = 'temp';
+  const branch = 'tina';
   const clientId = '0f0407c1-7703-4c55-8f49-f418087499df';
   const apiURL =
     process.env.NODE_ENV == "development"
