e185ae15a6f05c448b9e797cba3c44f0be3950ee
Jamie Baxter <baxterjrg@gmail.com>
2022-10-21 21:37:41 -0300

    main branch config

--- .tina/config.ts
+++ .tina/config.ts
@@ -2,7 +2,7 @@
 import { defineStaticConfig } from "tinacms";
 
 // Your hosting provider likely exposes this as an environment variable
-const branch = process.env.HEAD || "main";
+const branch = "main";
 
 export default defineStaticConfig({
   branch,
